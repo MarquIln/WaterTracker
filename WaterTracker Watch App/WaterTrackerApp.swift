1a8fe9bc324deab19161cc46dfe63cdcb887a4c5
Marcos Raach <m.raach@edu.pucrs.br>
2025-10-16 16:24:34 -0300

    Initial Commit

---
+++ WaterTracker Watch App/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker Watch App
+//
+//  Created by Marcos on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
